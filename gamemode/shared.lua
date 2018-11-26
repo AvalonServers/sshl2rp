ccc44265cd7d4277668d456db20acc2542739f55
Joseph Marsden <josephmarsden@towerdevs.xyz>
2018-11-26 01:41:55 +0000

    Initial commit

---
+++ gamemode/shared.lua
@@ -0,0 +1,10 @@
+DeriveGamemode("darkrp");
+
+GM.Name = "Semi-Serious HL2RP";
+GM.Author = "TOWER";
+GM.Email = "support@towerdevs.xyz";
+GM.Website = "https://towerdevs.xyz";
+
+function GM:Initialize()
+
+end
